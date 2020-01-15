eb88b9c1ba0b2abbeec26ed0b5dad6ec5fdd796e
Alexandre Krabbe <alekrabbe@gmail.com>
2020-01-15 16:51:52 -0400

    continua o curso

---
+++ services/passport.js
@@ -0,0 +1,38 @@
+import passport from "passport";
+import GoogleStrategy from "passport-google-oauth20";
+import mongoose from "mongoose";
+
+import keys from "../config/keys.js";
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy.Strategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id }).save().then(newUser => {
+            done(null, newUser);
+          });
+        }
+      });
+    }
+  )
+);
